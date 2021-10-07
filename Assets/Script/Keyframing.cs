58dfd45ec101bcc3bec7be55e9bcc12ae9cc1acf
Aditya Parmar <aditya.parmar@ontariotechu.net>
2021-10-06 20:41:23 -0400

    Improvised

---
+++ Assets/Script/Keyframing.cs
@@ -0,0 +1,96 @@
+/*
+ Jaden Hepburn Keyframing 100791169 Comp Algo Challenge 1
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyframing : MonoBehaviour
+{
+
+    public GameObject item;
+    public float waitingDuration = 2f;
+    public Vector3[] keyFrameArray;
+    public string styleType="position";
+
+    public void KeyFrame(Vector3[] keys, GameObject inputItem, float wD, string style)
+    {
+
+        switch (style)
+        {
+
+            case "position":
+                StartCoroutine(timingPosition());
+                IEnumerator timingPosition()
+                {
+
+                    for (int i = 0; i < keys.Length; i++)
+                    {
+                        inputItem.transform.position = keys[i];
+                        yield return new WaitForSeconds(wD);
+                        print("WAITING");
+
+                    }
+                }
+                break;
+
+            case "scale":
+                StartCoroutine(timingScale());
+                IEnumerator timingScale()
+                {
+
+                    for (int i = 0; i < keys.Length; i++)
+                    {
+                        inputItem.transform.localScale = keys[i];
+                        yield return new WaitForSeconds(wD);
+                        print("WAITING");
+
+                    }
+                }
+                break;
+
+            case "rotate":
+                StartCoroutine(timingRotate());
+                IEnumerator timingRotate()
+                {
+
+                    for (int i = 0; i < keys.Length; i++)
+                    {
+                        // inputItem.transform.rotation = keys[i];
+                        yield return new WaitForSeconds(wD);
+                        print("WAITING");
+
+                    }
+                }
+                break;
+
+
+
+        }
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3[] vec = { new Vector2(160, 200), new Vector2(160, 300), new Vector2(160, 375) };
+
+        KeyFrame(keyFrameArray, item, waitingDuration, styleType);
+    }
+
+
+    float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time += Time.deltaTime;
+
+    }
+
+
+}
